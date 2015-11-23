06795fdf6749507d6b3b8dc754caf3819162e0f9
rohitbemax <rohit.bemax@gmail.com>
2015-11-23 13:09:23 +0530

    Cleaned up dead code from test case

--- bidengine/src/main/java/test/BidderClient.java
+++ bidengine/src/main/java/test/BidderClient.java
@@ -20,28 +20,6 @@ public class BidderClient {
 		client = ClientBuilder.newClient();
 	}
 	
-	
-	public Bidder get(String id) {
-		
-		WebTarget target = client.target("http://localhost:8080/exercise-services/webapi/");
-		
-		Response response = target.path("activities/" + id).request(MediaType.APPLICATION_JSON).get(Response.class);
-		
-		if(response.getStatus() != 200) {
-			throw new RuntimeException(response.getStatus() + ": there was an error on the server.");
-		}
-		
-		return response.readEntity(Bidder.class);
-	}
-	
-	public List<User> get () {
-		WebTarget target = client.target("http://localhost:8080/exercise-services/webapi/");
-		
-		List<User> response = target.path("activities").request(MediaType.APPLICATION_JSON).get(new GenericType<List<User>>() {});
-		
-		return response;
-	}
-
 	public User create(User user) {
 		WebTarget target = client.target("http://localhost:8080/bidengine/webapi/");
 		
